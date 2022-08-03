193e50ef30a7583f8fa5355dc1c483eb5edf7957
henkling <105640757+henkling@users.noreply.github.com>
2022-08-03 10:23:16 +0300

    Create ff.py

---
+++ ff.py
@@ -0,0 +1,6 @@
+
+import subprocess
+domain = input('Enter the Domain: ')
+output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8') 
+output2 = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8') 
+output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8') 
