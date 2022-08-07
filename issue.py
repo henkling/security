4a9a36a62471b1030466d4019bcb350ed9502e63
henkling <105640757+henkling@users.noreply.github.com>
2022-08-07 16:37:09 +0300

    Update issue.py

--- issue.py
+++ issue.py
@@ -11,12 +11,17 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "positive1" {
+resource "aws_s3_bucket" "positive3" {
   bucket = "my-tf-test-bucket"
   acl    = "private"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
+  }
+
+  versioning {
+    enabled = true
+    mfa_delete = false
   }
 }
