b95a6271c0c271df5a7ace73f406c6f0c0a562df
henkling <105640757+henkling@users.noreply.github.com>
2022-08-09 15:43:01 +0300

    Update tf_file.tf

--- tf_file.tf
+++ tf_file.tf
@@ -1,5 +1,49 @@
-resource "aws_cloudtrail" "positive1" {
-  kms_key_id = "arn:aws:kms:<Your KMS Key>"
-  name                          = "npositive_1"
-  s3_bucket_name                = "bucketlog_1"
+provider "aws" {
+  region = "us-east-1"
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_cloudtrail" "foobar" {
+  name                          = "tf-trail-foobar"
+  s3_bucket_name                = aws_s3_bucket.foo.id
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = false
+}
+
+resource "aws_s3_bucket" "foo" {
+  bucket        = "tf-test-trail"
+  force_destroy = true
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::tf-test-trail/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
+Footer
