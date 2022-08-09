68534ca173470c706ef5ed17123c70df097b3923
henkling <105640757+henkling@users.noreply.github.com>
2022-08-09 14:54:34 +0300

    Delete h.py

--- h.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-domain = input('Enter the Domain: ')
-output = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8') 
-output2 = subprocess.check_output(f'nslookup {domain}', shell=True, encoding='UTF-8') 
